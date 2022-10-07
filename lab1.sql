deec777666e44bbc895eaa2936c45205f881717f
Tingcen <t33dong@uwaterloo.ca>
2022-10-07 12:00:26 -0400

    Update lab1.sql

--- lab1.sql
+++ lab1.sql
@@ -1,77 +1,47 @@
 ---- 1b
-SELECT job, count(job) AS count FROM Employee GROUP BY job ORDER BY job ASC;
+SELECT job, COUNT(*) as count FROM Employee group by job order by job asc;
 
 ---- 1e
-SELECT deptID
-FROM Employee
-WHERE job = 'engineer'
-GROUP BY deptID
-HAVING COUNT(job) >= (
-	SELECT MAX(cnt)
-	FROM (SELECT COUNT(job) AS cnt
-		FROM Employee
-		WHERE job = 'engineer'
-		GROUP BY deptID
-		) AS sub_cnt
-	)
-;
+SELECT deptID FROM (SELECT deptId, COUNT(*) as cnt from Employee where job = "engineer" group by deptID order by COUNT(*) desc) as T
+where cnt = (SELECT COUNT(*) as cnt from Employee where job = "engineer" group by deptID order by COUNT(*) desc LIMIT 1);
+
 
 ---- 1g
-select empID from Employee where salary in (select max(salary) from Employee where salary < (select max(salary) from Employee));
+SELECT empID from Employee where salary = (SELECT salary from Employee where salary < (SELECT salary from Employee order by salary desc LIMIT 1) order by salary desc LIMIT 1);
+
 
 ---- 2a
-select empName, empID from Employee where empID not in (select empID from Assigned);
+SELECT Employee.empName, Employee.empID FROM Employee WHERE Employee.empID NOT IN (SELECT empID FROM Assigned);
+
 
 ---- 2e
-select sum(salary) as projectSalary, projID from Employee natural left outer join Assigned group by projID;
+SELECT projID, SUM(salary) as projectSalary from (Employee natural join Assigned) group by projID union SELECT NULL, SUM(salary) FROM (Employee) WHERE Employee.empID NOT IN (SELECT empID FROM Assigned);
 
 ---- 3a
-UPDATE 
-	Employee AS emp 
-	NATURAL JOIN Assigned
-	NATURAL JOIN Project AS proj 
-SET 
-	emp.salary = emp.salary * 1.10
-WHERE
-	proj.title = 'compiler'
-;
+update Employee set salary = salary * 1.1 WHERE empID IN (SELECT empID from Project JOIN Assigned WHERE Project.projID = Assigned.projID and Project.title = "compiler");
+
 
 ---- 3b
-UPDATE 
-	Employee AS emp
-	NATURAL JOIN Department AS dept
-SET 
-	emp.salary = emp.salary * 
-	CASE 
-    WHEN dept.location = 'Waterloo' THEN 1.08 
-    WHEN emp.job = 'janitor' THEN 1.05
-    ELSE 1
-    END
-WHERE
-	dept.location = 'Waterloo'
-    OR emp.job = 'janitor'
-;
+update (Employee natural join Department)
+set salary = salary * 
+(case
+when location = "Waterloo" and job = "janitor" then 1.08
+when job = "janitor" then 1.05
+when location = "Waterloo" then 1.08
+else 1
+end);
 
 
 ---- 3c
 ALTER TABLE Employee
-ADD shift VARCHAR(5)
-;
+ADD shift VARCHAR(5);
 
 
 ---- 3d
-UPDATE 
-	Employee AS emp
-SET 
-	emp.shift = 
-	CASE 
-    WHEN NOT empID IN (
-		SELECT empID
-        FROM Assigned
-	) 
-    THEN 'N.A.'
-    WHEN emp.empID % 2 = 0 THEN 'DAY'
-    ELSE 'NIGHT'
-    END
-;
-
+update (Employee natural join Assigned)
+set shift = 
+(case
+when projID = null then "N.A."
+when MOD(empID, 2) = 0 then "DAY"
+when MOD(empID, 2) != 0 then "NIGHT"
+end);
